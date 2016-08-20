93c9667c8d0e05bdb1c8d984d421bdb78fe29d75
Fred <fduehsler@gmail.com>
2016-08-20 15:47:48 +0200

    added jquery

---
+++ js/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+
+ $('#button1').on('click',function() {
+
+   $('.box1').toggle('slow');
+
+
+ });
+ $('#button2').on('click',function() {
+
+   $('.textblock').toggle('slow');
+
+
+ });
+});
